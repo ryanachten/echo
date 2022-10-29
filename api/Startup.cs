9b78edd797209d8ebeda9d7f203e26358b82df60
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2022-10-29 10:14:53 -0700

    feat: add init client dockerisation

--- api/Startup.cs
+++ api/Startup.cs
@@ -33,9 +33,11 @@ namespace Echo
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            var allowedOrigins = Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
             app.UseCors(builder =>
             {
-                builder.WithOrigins("http://localhost:3000")
+                builder.WithOrigins(allowedOrigins)
                     .AllowAnyHeader()
                     .WithMethods("GET", "POST")
                     .AllowCredentials();
